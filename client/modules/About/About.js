c7de8ef0ba160df7ca121642102a211e1201f67c
Mariusz Domanski <mariusz@domanski.pl>
2019-05-23 19:52:23 +0200

    Create about us and posts

---
+++ client/modules/About/About.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About Us!</h2>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+About.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
